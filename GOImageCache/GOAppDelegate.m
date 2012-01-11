6158d817c4fb0d1a30f012f863fcfa35d78aaefc
Samuel Goodwin <goodwinlabs@gmail.com>
2012-01-10 21:23:42 -0500

    Silly copyright template lines.

--- GOImageCache/GOAppDelegate.m
+++ GOImageCache/GOAppDelegate.m
@@ -3,7 +3,6 @@
 //  GOImageCache
 //
 //  Created by Samuel Goodwin on 12/30/11.
-//  Copyright (c) 2011 SNAP Interactive. All rights reserved.
 //
 
 #import "GOAppDelegate.h"
--- GOImageCache/GOBlackOutView.m
+++ GOImageCache/GOBlackOutView.m
@@ -3,7 +3,6 @@
 //  GOImageCache
 //
 //  Created by Samuel Goodwin on 12/31/11.
-//  Copyright (c) 2011 SNAP Interactive. All rights reserved.
 //
 
 #import "GOBlackOutView.h"
